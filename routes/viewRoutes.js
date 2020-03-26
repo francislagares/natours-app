ab3c0d151750e91f81b94c8edac6a6ce1b4e985a
Francis Lagares <francis.lagares@gmail.com>
2020-03-26 21:39:12 +0100

    Fixed route bug

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
 router.use(viewsController.alerts);
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewsController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLogin);
 router.get('/me', authController.protect, viewsController.getAccount);
