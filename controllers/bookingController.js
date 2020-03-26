720e25379f50e1c9c8610468f224a00f491c59ef
Francis Lagares <francis.lagares@gmail.com>
2020-03-26 21:40:59 +0100

    Renamed line_items property to display_items

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -17,7 +17,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    line_items: [
+    display_items: [
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
